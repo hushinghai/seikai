ce3bffe0ce24d5f523e1b27744222e21cb4fd23d
Vishnu Surendran <vishnu.ns@provenlogic.net>
2015-08-21 18:28:35 +0530

    pandya changes

--- app/views/pageshell.blade.php
+++ app/views/pageshell.blade.php
@@ -46,6 +46,13 @@
   <link id="base-style-responsive" href="{{ URL::asset('web/Content/base_styles.css') }}" rel="stylesheet">
   <link id="base-style-responsive" href="{{ URL::asset('web/Content/home.css') }}" rel="stylesheet">
   <link rel="stylesheet"  href="{{ URL::asset('web/Content/superfish.css') }}"   media="screen">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{asset('uploads/app/apple-touch-icon-144-precomposed.png')}}">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{asset('uploads/app/apple-touch-icon-114-precomposed.png')}}">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{asset('uploads/app/apple-touch-icon-72-precomposed.png')}}">
+  <link rel="apple-touch-icon-precomposed" href="{{asset('uploads/app/apple-touch-icon-57-precomposed.png')}}">
+  <link rel="shortcut icon" href="{{asset('uploads/app/favicon.png')}}">
+  <link rel="shortcut icon" href="{{asset('uploads/app/favicon.ico')}}">
+
   
 <!--[if IE ]> 
 <style>
